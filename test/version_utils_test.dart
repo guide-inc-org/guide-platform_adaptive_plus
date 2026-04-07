615c36d73a7dee47658e829518469ef9860d5faf
SYND <synd@guide.inc>
2026-04-07 19:01:49 +0700

    [v1.0] Update package name

--- test/version_utils_test.dart
+++ test/version_utils_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:platform_adaptive/utils/version_utils.dart';
+import 'package:platform_adaptive_plus/utils/version_utils.dart';
 
 void main() {
   group('VersionUtils.getMajorVersion', () {
